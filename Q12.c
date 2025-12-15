5a70bd930f8c6dd3784473201c4cffed2fd94bcc
lava-2525 <lava.gupta2525@gmail.com>
2025-12-15 22:58:35 +0530

    Add score input and min/max calculation
    This program reads a number of scores from the user and calculates the maximum and minimum scores.

---
+++ Q12.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i;
+    
+    printf("Enter number of scores: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter %d scores:\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(i=1;i<n;i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum Score = %d\n",max);
+    printf("Minimum Score = %d\n",min);
+
+    return 0;
+}
